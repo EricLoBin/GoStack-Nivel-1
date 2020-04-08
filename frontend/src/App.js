907f90c2cb66cac4e1c27d358209b72dd7ee0bc4
EricLoBin <ekilobin@gmail.com>
2020-04-08 18:35:49 -0300

    [ADD/UPDATE/REMOVE] Listando Projetos da API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 
@@ -23,8 +23,6 @@ function App() {
     <>
         <Header title="Projects" />
 
-        <img width={300} src={backgroundImage} alt="background Image"/>
-        
         <ul>
             {projects.map(project => <li key={project}>{project}</li>)}
         </ul>
