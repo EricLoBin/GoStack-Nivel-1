d4ad7bac2611e992f34d4ef12964cd511458dc39
EricLoBin <ekilobin@gmail.com>
2020-04-09 18:55:07 -0300

    [UPDATE] Criando novos projetos

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -17,11 +17,24 @@ export default function App() {
         api.get('projects').then(response => {
             setProjects(response.data);
         });
-    }, [])
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: 'Eric'
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
 
 
     return (
         <>
+            <StatusBar barStyle="light-content" backgroundColor="#4F379F" />
+
             <SafeAreaView style={styles.container} >
                 <FlatList
                     data={projects}
@@ -30,6 +43,15 @@ export default function App() {
                         <Text style={styles.project} >{project.title}</Text>
                     )}
                 />
+        
+                <TouchableOpacity
+                    activeOpacity={0.6}
+                    style={styles.button}
+                    onPress={handleAddProject}
+                >
+                    <Text style={styles.buttonText}>Adicionar projeto</Text>
+                </TouchableOpacity>
+
             </SafeAreaView>
         </>
     );
@@ -49,4 +71,16 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 30,
     },
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
 });
