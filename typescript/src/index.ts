045c9a2e1eefe203461db0cb2dd47d513c67acb4
EricLoBin <ekilobin@gmail.com>
2020-04-10 15:49:41 -0300

    [ADD/UPDATE] Configurando o projeto

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello TypeScripr' });
+});
+
+app.listen(3333);
